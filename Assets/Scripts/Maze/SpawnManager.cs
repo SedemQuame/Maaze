43becbb3e01bc7bd52734b76c2dc0aed1642831f
SedemQuame <sedem.anekpewu.3@gmail.com>
2021-05-25 07:28:10 +0000

    updated the spawn manager to spawn enemies at a random locations.

--- Assets/Scripts/Maze/SpawnManager.cs
+++ Assets/Scripts/Maze/SpawnManager.cs
@@ -12,11 +12,20 @@ public class SpawnManager : MonoBehaviour
     }
 
     void spawnEnemiesByLevelDifficulty(){
+        // todo, find a smarter way of spawning enemies such that the enemies are not all spawned at once?
         int enemyType = Random.Range(0, 2);
         for (int i = 0; i < (LevelDifficulty.levelDifficulty + 1); i++)
         {
             if(LevelDifficulty.levelDifficulty == 1) return;
-            spawn(enemyArr[enemyType], new Vector3(0,0,0));
+            // spawn objects in random cells.
+
+            GameObject mazeLoader = GameObject.Find("Maze Loader Holder");
+            MazeLoader loader = mazeLoader.GetComponent<MazeLoader>();
+
+            string lastCreatedCell = "Floor " + (Random.Range(1, loader.getRowAndColumnNumber())) + "," + (Random.Range(0, loader.getRowAndColumnNumber()));
+
+            Vector3 playerJumpOffset = new Vector3(0, 10, 0);
+            spawn(enemyArr[enemyType], GameObject.Find(lastCreatedCell).transform.position + playerJumpOffset);
         }
     }
 
