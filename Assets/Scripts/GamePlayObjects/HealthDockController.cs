4e886b10e63d2dbf2c60b7c4ad874f069ca1689b
SedemQuame <sedem.amekpewu.3@gmail.com>
2021-08-07 01:06:07 +0100

    added function to heal progressively by stagressively by standing on the healing pad.

--- Assets/Scripts/GamePlayObjects/HealthDockController.cs
+++ Assets/Scripts/GamePlayObjects/HealthDockController.cs
@@ -6,27 +6,50 @@ public class HealthDockController : MonoBehaviour
 {
     // this should be in increments of 2 to 10.
     public int healthIncrementValue;
-    public float healthIncrementRate = 0.1f;
+    public GameObject player;
+    [SerializeField]
+    private float healthIncrementRate = 0.1f;
+    [SerializeField]
+    private bool isStandingOnHealingPad = false, isCoroutineRunning = false, isPlayer = false;
+    [SerializeField]
+    private Collider collidingGameObject = null;
+
+    void Start(){
+        player = GameObject.Find("Player");
+    }
+
+    void Update(){
+        if(collidingGameObject != null && isStandingOnHealingPad){
+            if(!isCoroutineRunning){
+                StartCoroutine("restoreHealth");
+            }
+        }else{
+            StopCoroutine("restoreHealth");
+            isCoroutineRunning = false;
+        }
+    }
 
     void OnTriggerEnter(Collider other){
-        StartCoroutine(restoreHealth(other));
+        collidingGameObject = other;
+        if(collidingGameObject.tag == "Player"){
+            isPlayer = true;
+        }
+        isStandingOnHealingPad = true;
     }
 
     void OnTriggerExit(Collider other){
-        StopCoroutine(restoreHealth(other));
+        collidingGameObject = other;
+        if(collidingGameObject.tag == "Player"){
+            isPlayer = false;
+        }
+        isStandingOnHealingPad = false;
     }
 
-    IEnumerator restoreHealth(Collider other){
-        // increment the health value.
-        yield return new WaitForSeconds(healthIncrementRate);
-        if(other.gameObject.tag == "Player" || other.gameObject.tag == "Nozzle"){
-            // negate health point when passing to (-- => +).
-            if(other.gameObject.tag == "Nozzle"){
-                other.gameObject.GetComponentInParent<PlayerController>().updateHealthBar(-healthIncrementValue);
-            }else{
-                other.gameObject.GetComponent<PlayerController>().updateHealthBar(-healthIncrementValue);
-            }
+    IEnumerator restoreHealth(){
+        isCoroutineRunning = true;
+        while(isPlayer){
+            player.GetComponentInParent<PlayerController>().updateHealthBar(-healthIncrementValue);
+            yield return new WaitForSeconds(0.4f);
         }
-        yield return null;
     }
 }
